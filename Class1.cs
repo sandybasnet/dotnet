f965d3199a4805c87a88d22dfc55150b47edfd50
sandybasnet <sandeshbasnet773@gmail.com>
2024-05-15 23:12:34 +0545

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+        int i; bool a;
+        static void Main() 
+        {
+            Class1 p = new ConsoleApp2.Class1();
+            Console.WriteLine(p.a);
+            Console.WriteLine(p.i);
+            Console.WriteLine("Second Hello");
+        }
+    }
+}
---
+++ ExplicitCon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class ExplicitCon
+    {
+        public ExplicitCon() //Defining the constructor
+        {
+            Console.WriteLine("Constructor is called");
+        }
+
+        static void Main()
+        {
+            ExplicitCon obj = new ExplicitCon(); //Calling the constructor
+            ExplicitCon obj1 = new ExplicitCon();
+            Console.ReadLine();
+        }
+    }
+}
